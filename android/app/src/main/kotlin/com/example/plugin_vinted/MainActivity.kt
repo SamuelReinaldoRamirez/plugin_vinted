b1a2809d8e016a5ec248f225a0bf697f5496e764
SamuelReinaldoRamirez <samuel.reinaldo.ramirez@gmail.com>
2024-10-31 12:55:58 +0100

    rond rouge en premier plan

--- android/app/src/main/kotlin/com/example/plugin_vinted/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/plugin_vinted/MainActivity.kt
@@ -1,5 +1,40 @@
 package com.example.plugin_vinted
 
+import android.content.Intent
+import android.os.Bundle
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "overlay_channel"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "showOverlay") {
+                startOverlayService()
+                result.success(null)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun startOverlayService() {
+        val intent = Intent(this, OverlayService::class.java)
+        startService(intent)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopOverlayService() // Arrêter le service overlay lorsque l'application est fermée
+    }
+
+    private fun stopOverlayService() {
+        val intent = Intent(this, OverlayService::class.java)
+        stopService(intent)
+    }
+}
